05ce4ed6d77edf5022d94aba7ace39a3596ec416
VINAYOMKAR9 <vinayomkar9@gmail.com>
2024-07-25 09:58:39 +0530

    react

--- useeffectapp/src/App.js
+++ useeffectapp/src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 import Timer from './components/Timer';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const[show,setShow]= useState(false)
+
   return (
     <div className="App">
-      <Timer/>
+
+      <button onClick={()=> setShow((pr)=>!pr)}>
+        {!show ? 'Show':'Hide'}
+      </button>
+      {show && <Timer/>}
     </div>
   );
 }
--- useeffectapp/src/components/Timer.jsx
+++ useeffectapp/src/components/Timer.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, {useEffect,useState } from 'react'
 
 const Timer = () => {
-  return (
-    <div>
-      Time
+    const[timer,setTimer]=useState(0)
+    const[intervalId,setIntervalId]= useState(null)
+    const[isRunning, setIsRunning]= useState(false)
+
+
+useEffect(()=>{
+      if(isRunning){let id = setInterval(()=>{
+        setTimer(prev => prev+1)
+        console.log('running..')
+      },1000)
+      setIntervalId(id)
+    }else{
+        clearInterval(intervalId)
+        setIntervalId(null)
+    }
+      return ()=>{
+        console.log('unMounting...')
+        clearInterval(intervalId)
+      }
+  
+    },[isRunning])
+
+    const handleStart=()=>{
+        setIsRunning(true)    
+
+    }
+    const handleStop=()=>{
+        setIsRunning(false)
+    }
+    const handleReset=()=>{
+        setIsRunning(false)
+        setTimer(0)
+
+    }
+
+    return(<div>
+        <h2>  Timer:{timer}</h2>
+        <button onClick={handleStart} disabled={isRunning}>Start</button>
+        <button onClick={handleStop} disabled={!isRunning}>Stop</button>
+        <button onClick={handleReset}>Reset</button>
     </div>
-  )
+    )
 }
 
 export default Timer
